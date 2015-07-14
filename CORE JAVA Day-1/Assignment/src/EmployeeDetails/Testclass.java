1e72fae560d191429de42f5fe1c71a9d87302d36
meenakshik-optimus <meenakshi.kumari@optimusinfo.com>
2015-07-14 17:49:46 +0530

    JAVA INDUCTION DAY ONE

--- CORE JAVA Day-1/Assignment/src/EmployeeDetails/Testclass.java
+++ CORE JAVA Day-1/Assignment/src/EmployeeDetails/Testclass.java
@@ -6,7 +6,7 @@ package EmployeeDetails;
  * @author optimus157
  *
  */
-public class Testclass {
+public class TestClass {
 
 	/**
 	 * main() method starts
@@ -18,87 +18,87 @@ public class Testclass {
 		/**
 		 * objects of Employee class are created
 		 */
-		Employee E1 = new Employee("Sumit", 1000);
-		Employee E2 = new Employee("Nitish", 500);
-		Employee E3 = new Employee("Abhay");
-		Employee E4 = new Employee("Parveen");
+		Employee sumit = new Employee("Sumit", 1000);
+		Employee nitish = new Employee("Nitish", 500);
+		Employee abhay = new Employee("Abhay");
+		Employee parveen = new Employee("Parveen");
 
 		/**
 		 * setting the salary details of employees
 		 */
-		E1.setDetails(10000, 2500, 1500);
-		E2.setDetails(12000, 3500, 2000);
-		E3.setDetails(20000, 3500, 2500);
-		E4.setDetails(30000, 4500, 2000);
+		sumit.setDetails(10000, 2500, 1500);
+		nitish.setDetails(12000, 3500, 2000);
+		abhay.setDetails(20000, 3500, 2500);
+		parveen.setDetails(30000, 4500, 2000);
 
 		/**
 		 * calling getName() and calculate() method on employee objects
 		 */
-		System.out.println(" Salary of " + E1.getName() + " is "
-				+ E1.calculate());
-		System.out.println(" Salary of " + E2.getName() + " is "
-				+ E2.calculate());
-		System.out.println(" Salary of " + E3.getName() + " is "
-				+ E3.calculate());
-		System.out.println(" Salary of " + E4.getName() + " is "
-				+ E4.calculate());
+		System.out.println(" Salary of " + sumit.getName() + " is "
+				+ sumit.calculate());
+		System.out.println(" Salary of " + nitish.getName() + " is "
+				+ nitish.calculate());
+		System.out.println(" Salary of " + abhay.getName() + " is "
+				+ abhay.calculate());
+		System.out.println(" Salary of " + parveen.getName() + " is "
+				+ parveen.calculate());
 
 		/**
 		 * counting the leave before taking the leave
 		 */
-		System.out.println(" Leaves left in " + E1.getName() + "'s account is "
-				+ E1.countLeaves());
+		System.out.println(" Leaves left in " + sumit.getName() + "'s account is "
+				+ sumit.countLeaves());
 
 		/**
-		 * Employee E1 took leave for 2 days
+		 * Employee sumit took leave for 2 days
 		 */
-		E1.takeLeave(2);
-
-		/**
-		 * calling countLeaves() method to count the leaves for employee
-		 */
-		System.out.println(" Leaves left in " + E2.getName() + "'s account is "
-				+ E2.countLeaves());
-
-		/**
-		 * Employee E1 took leave for 4 days
-		 */
-		E2.takeLeave(4);
+		sumit.takeLeave(2);
 
 		/**
 		 * calling countLeaves() method to count the leaves for employee
 		 */
-		System.out.println(" Leaves left in " + E1.getName() + "'s account is "
-				+ E1.countLeaves());
+		System.out.println(" Leaves left in " + nitish.getName() + "'s account is "
+				+ nitish.countLeaves());
+
+		/**
+		 * Employee nitish took leave for 4 days
+		 */
+		nitish.takeLeave(4);
+
+		/**
+		 * calling countLeaves() method to count the leaves for employee
+		 */
+		System.out.println(" Leaves left in " + sumit.getName() + "'s account is "
+				+ sumit.countLeaves());
 
 		/**
 		 * calling takeLeave() method
 		 */
-		E1.takeLeave(2);
+		sumit.takeLeave(2);
 
 		/**
-		 * using refeernce variable of Abscalculation class but the objects are
+		 * using refeernce variable of GenericEmployee class but the objects are
 		 * different
 		 */
-		GenericEmployee ab;
+		GenericEmployee generic;
 
 		/**
 		 * assigning object E1 and E2 to ab one by one for the purpose of
 		 * polymorphism
 		 */
-		ab = E1;
-		System.out.println(" Salary of " + E1.getName() + " is "
-				+ ab.calculate());
-		ab = E2;
-		System.out.println(" Salary of " + E2.getName() + " is "
-				+ ab.calculate());
+		generic = sumit;
+		System.out.println(" Salary of " + sumit.getName() + " is "
+				+ generic.calculate());
+		generic = nitish;
+		System.out.println(" Salary of " + nitish.getName() + " is "
+				+ generic.calculate());
 
 		/**
 		 * calling overloaded countLeaves() method
 		 */
-		E1.countLeaves("Sumit");
-		E1.takeLeave(12);
-		E2.countLeaves("Nitish");
-		E2.takeLeave(5);
+		sumit.countLeaves("Sumit");
+		sumit.takeLeave(12);
+		nitish.countLeaves("Nitish");
+		nitish.takeLeave(5);
 	}
 }
