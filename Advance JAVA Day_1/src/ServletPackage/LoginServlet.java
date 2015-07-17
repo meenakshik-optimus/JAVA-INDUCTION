84b199d200c0564c737dd2bf176a028370e4e648
meenakshik-optimus <meenakshi.kumari@optimusinfo.com>
2015-07-17 18:55:19 +0530

    JAVA-INDUCTION

---
+++ Advance JAVA Day_1/src/ServletPackage/LoginServlet.java
@@ -0,0 +1,48 @@
+package ServletPackage;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("Name");
+		String pass = request.getParameter("Password");
+		if ((name.equals("mixi") && pass.equals("mixi"))||(name.equals("meenu") && pass.equals("meenu"))) {
+			RequestDispatcher requestDispathcher = request
+					.getRequestDispatcher("HitCount");
+			requestDispathcher.forward(request, response);
+		} else {
+			RequestDispatcher requestDispathcher = request
+					.getRequestDispatcher("/Login.html");
+			requestDispathcher.include(request, response);
+		}
+	}
+
+}
