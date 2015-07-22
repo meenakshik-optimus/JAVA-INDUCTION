9a211598990681c5d80e0bb7391d8e72d0e06d2b
meenakshik-optimus <meenakshi.kumari@optimusinfo.com>
2015-07-22 09:02:10 +0530

    hibernate

--- Hibernate/src/edu/javalearn/domain/hibernate/Application.java
+++ Hibernate/src/edu/javalearn/domain/hibernate/Application.java
@@ -2,30 +2,30 @@ package edu.javalearn.domain.hibernate;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
-
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 public class Application {
 	public static void main(String[] args) {
 
 		/**
 		 * creating session factory object
 		 */
-		SessionFactory sessionFactory = new AnnotationConfiguration()
-				.configure().buildSessionFactory();
-
+		Configuration configuration=new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		SessionFactory sessionFactory =configuration.buildSessionFactory();
+		
 		/**
 		 * creating session object
 		 */
 		Session session = sessionFactory.openSession();
-
-		session.beginTransaction();
+		Transaction transaction=session.beginTransaction();
 		Employee sumit = new Employee();
 		sumit.setId(1);
 		sumit.setFirstName("Sumit");
 		sumit.setLastName("sherwal");
 
 		session.save(sumit);
-		session.getTransaction().commit();
+		transaction.commit();
 		session.close();
 
 		System.out.println("successfully saved");
